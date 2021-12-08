ed8c22270b1cbf7d031b658cf786364b0000e2c3
Qonita Nur Iffat <qonita.nuriffat10@gmail.com>
2021-12-08 22:55:10 +0700

    feat: modified WebSecurityConfig

--- sibusiness/src/main/java/apap/tugasakhir/sibusiness/security/WebSecurityConfig.java
+++ sibusiness/src/main/java/apap/tugasakhir/sibusiness/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/user/**").hasAuthority("Manager Business")
+                .antMatchers(HttpMethod.POST, "/api/v1/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -35,13 +38,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    /*@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("pebisnispemula").password(encoder().encode("sibusiness"))
-                .roles("Manager Business");
-    }*/
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(encoder())
+//                .withUser("pebisnispemula").password(encoder().encode("sibusiness"))
+//                .roles("Manager Business");
+//    }
 
     @Autowired
     private UserDetailsService userDetailsService;

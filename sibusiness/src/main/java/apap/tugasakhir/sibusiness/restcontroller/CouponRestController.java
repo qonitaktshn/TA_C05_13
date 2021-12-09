ab9356cc6ce3a73c281714a336c5dff15bae2a3a
Raihan Rizqi Muhtadiin <55452897+RaiRiz@users.noreply.github.com>
2021-12-09 17:41:39 +0800

    Feat/milestone2 raihan fitur17 (#29)
    * chore: troubleshoot deployment
    
    * feat: change table name for UserModel from 'user' to 'pengguna' to avoid conflict with postgreSQL database on deployment
    
    * chore: resolve springbootdevtools double dependency problems
    
    * chore: try to resolve deploy problem
    
    * chore: trying to resolve deploy problems
    
    * chore: fix resolve pom.xml
    
    * refactor: menyesuaikan spec database dengan kebutuhan soal
    
    * feat: membuat model untuk entity Coupon dan CouponType
    
    * refactor: change hidden th:field from id to uuid
    
    * refactor: resolve Json infinite recursion problem
    
    * refactor: open access to coupon API with basic auth
    
    * feat: implementasi fitur 17
    
    * refactor: add min and max constraint for discountAmount
    
    * refactor: implement BaseResponse for better webservice doc
    
    * refactor: tidying up

--- sibusiness/src/main/java/apap/tugasakhir/sibusiness/restcontroller/CouponRestController.java
+++ sibusiness/src/main/java/apap/tugasakhir/sibusiness/restcontroller/CouponRestController.java
@@ -1,6 +1,7 @@
 package apap.tugasakhir.sibusiness.restcontroller;
 
 import apap.tugasakhir.sibusiness.model.CouponModel;
+import apap.tugasakhir.sibusiness.rest.BaseResponse;
 import apap.tugasakhir.sibusiness.restservice.CouponRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,11 @@ public class CouponRestController {
     private CouponRestService couponRestService;
 
     @GetMapping(value = "/coupon/allCoupon")
-    private List<CouponModel> getListCodedCoupon() {
-        return couponRestService.getListCodedCoupon();
+    private BaseResponse<List<CouponModel>> getListCodedCoupon() {
+        BaseResponse<List<CouponModel>> response = new BaseResponse<>();
+        response.setStatus(200);
+        response.setMessage("success");
+        response.setResult(couponRestService.getListCodedCoupon());
+        return response;
     }
 }

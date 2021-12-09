70754a389b87d0eaeb2168c66390267eb97f13fd
Qonita Nur Iffat <qonita.nuriffat10@gmail.com>
2021-12-09 22:43:29 +0700

    feat: add mesinURL

--- sibusiness/src/main/java/apap/tugasakhir/sibusiness/rest/Setting.java
+++ sibusiness/src/main/java/apap/tugasakhir/sibusiness/rest/Setting.java
@@ -2,4 +2,5 @@ package apap.tugasakhir.sibusiness.rest;
 
 public class Setting {
     final public static String itemURL = "https://si-item.herokuapp.com";
+    final public static String mesinURL = "https://cc47fcc5-d5fc-4550-ba4b-525da319e572.mock.pstmn.io";
 }
